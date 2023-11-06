added separate calculation for functional lcc size + some visualization fixes

frontend.py
@@ -36,11 +36,15 @@ lcc_size = percolation.get_lcc_size(sim_net)
 
 cfg['use_cylindrical_coords'] = False
 net_cleaned['pore.diameter'] = sim_net['pore.diameter']
-effective_conductances, lcc_sizes = percolation.run_percolation(net_cleaned, cfg, percolation_type='bond', removal_order='random')
+effective_conductances, lcc_sizes, functional_lcc_sizes = percolation.run_percolation(net_cleaned, cfg, percolation_type='bond', removal_order='random')
 np.append(np.array([effective_conductance]), effective_conductances)
 np.append(np.array([lcc_size]), lcc_sizes)
-percolation_outcome_values = np.concatenate((np.expand_dims(effective_conductances, axis=0), np.expand_dims(lcc_sizes, axis=0)), axis=0)
+np.append(np.array([lcc_size]), functional_lcc_sizes)
+percolation_outcome_values = np.concatenate((np.expand_dims(effective_conductances, axis=0), 
+                                             np.expand_dims(lcc_sizes, axis=0), np.expand_dims(functional_lcc_sizes, axis=0)),
+                                             axis=0)
 visualization.plot_percolation_curve(net_cleaned['throat.conns'].shape[0], percolation_outcome_values,
                                      colors=params.percolation_outcome_colors, labels=params.percolation_outcome_labels, 
-                                     alphas=params.percolation_outcome_alphas, y_labels=params.percolation_outcome_ylabels)
+                                     alphas=params.percolation_outcome_alphas, y_labels=params.percolation_outcome_ylabels,
+                                     axindex=params.percolation_outcome_axindex, save_path=params.percolation_plot_save_path)
 

percolation.py
@@ -60,7 +60,10 @@ def run_percolation(net, cfg, percolation_type='bond', removal_order='random'):
         effective conductance of the network after each removal
     lcc_size : np.array
         size of the largest connected component after each removal
+    functional_lcc_size : np.array
+        the size of the largest functional component (i.e. component connected to both inlet and outlet) after each removal
     """
+    #import pdb; pdb.set_trace()
     assert percolation_type in ['bond', 'site'], 'percolation type must be bond (removal of throats) or site (removal of pores)'
     if percolation_type == 'bond':
         n_removals = net['throat.conns'].shape[0]
@@ -70,6 +73,7 @@ def run_percolation(net, cfg, percolation_type='bond', removal_order='random'):
     
     effective_conductances = np.zeros(n_removals)
     lcc_size = np.zeros(n_removals)
+    functional_lcc_size = np.zeros(n_removals)
     cfg['conduit_diameters'] = 'inherit_from_net'
     if removal_order == 'random':
         removal_order = np.arange(0, n_removals)
@@ -79,21 +83,27 @@ def run_percolation(net, cfg, percolation_type='bond', removal_order='random'):
         sim_net['throat.type'] = net['throat.type']
         if 'pore.diameter' in net.keys():
             sim_net['pore.diameter'] = net['pore.diameter']
-        try:
-            if percolation_type == 'bond':
-                op.topotools.trim(sim_net, throats=removal_order[:i + 1])
-            elif percolation_type == 'site':
+        if percolation_type == 'bond':
+            op.topotools.trim(sim_net, throats=removal_order[:i + 1])
+        elif percolation_type == 'site':
+            try:
                 op.topotools.trim(sim_net, pores=removal_order[:i + 1])
+            except Exception as e:
+                if str(e) == 'Cannot delete ALL pores':
+                    continue # this would remove the last node from the network; at this point, value of all outputs should be 0
+        lcc_size[i] = get_lcc_size(sim_net)
+        try:
             conduit_elements = mrad_model.get_conduit_elements(sim_net, cec_indicator=cec_indicator)
             sim_net = mrad_model.clean_network(sim_net, conduit_elements, cfg['net_size'][0] - 1, remove_dead_ends=False)
             sim_net = mrad_model.prepare_simulation_network(sim_net, cfg)
             effective_conductances[i] = mrad_model.simulate_water_flow(sim_net, cfg, visualize=False)
-            lcc_size[i] = get_lcc_size(sim_net)
-        except:
-            effective_conductances[i] = 0
-            lcc_size[i] = 0
+            functional_lcc_size[i] = get_lcc_size(sim_net)
+        except Exception as e:
+            if str(e) == 'Cannot delete ALL pores': # this is because all remaining nodes belong to non-functional components
+                effective_conductances[i] = 0  
+                functional_lcc_size[i] = 0
         
-    return effective_conductances, lcc_size
+    return effective_conductances, lcc_size, functional_lcc_size
 
 def get_lcc_size(net):
     """
@@ -110,7 +120,14 @@ def get_lcc_size(net):
         size of the largest connected component
     """
     # TODO: check what this returns if there are no nodes in the network; should return 0
-    A = net.create_adjacency_matrix(fmt='coo', triu=True) # the rows/columns of A correspond to conduit elements and values indicate the presence/absence of connections
+    try:
+        A = net.create_adjacency_matrix(fmt='coo', triu=True) # the rows/columns of A correspond to conduit elements and values indicate the presence/absence of connections
+    except KeyError as e:
+        if str(e) == "'throat.conns'":
+            if len(net['throat.all']) == 0:
+                A = np.zeros((len(net['pore.coords']), len(net['pore.coords'])))
+            else:
+                raise
     component_labels = csg.connected_components(A, directed=False)[1]
     component_sizes = [len(np.where(component_labels == component_label)[0]) for component_label in np.unique(component_labels)]
     lcc_size = np.amax(component_sizes)

visualization.py
@@ -92,7 +92,7 @@ def make_colored_pore_scatter(net, pore_values, title='', cmap=plt.cm.jet):
     fig.colorbar(p, ax=ax)
     plt.title(title)
     
-def plot_percolation_curve(total_n_nodes, values, colors=[], labels=[], alphas=[], y_labels=[]):
+def plot_percolation_curve(total_n_nodes, values, colors=[], labels=[], alphas=[], axindex=[], y_labels=[], save_path=''):
     """
     Plots the percolation analysis outcomes (largest connected component size and effective conductance) as a function
     of the fraction of nodes removed.
@@ -111,10 +111,15 @@ def plot_percolation_curve(total_n_nodes, values, colors=[], labels=[], alphas=[
         colors used for plotting. len(colors) should equal to values.shape[0]
     labels : list of strs, optional
         labels of the curves plotted. len(labels) should equal to values.shape[0]
-    alphas : iterable of floats
+    alphas : iterable of floats, optional
         transparency values of the plotted lines. len(alphas) should equal to values.shape[0]
+    axindex : iterable of ints, optional
+        index of the y axis (first or secondary), on which the corresponding row of values should be drawn. len(axindex) should equal to values.shape[0]
     y_labels : list of strs, optional
-        labels of the first and secondary y axis. only used if values.shape[0] == 2, in which case len(y_labels) should be 2
+        labels of the first and secondary y axis. only used if the secondary y axis is used, in which case len(y_labels) should be 2
+    save_path : str, optional
+        if len(save_path) > 0, the figure will be saved as .pdf to the given path
+        
 
     Returns
     -------
@@ -132,17 +137,25 @@ def plot_percolation_curve(total_n_nodes, values, colors=[], labels=[], alphas=[
     
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    if values.shape[0] == 2:
+    
+    if len(np.unique(axindex)) > 1: #secondary y axis will be used 
         ax2 = ax.twinx()
-        ax.plot(x, values[0,:], color=colors[0], label=labels[0], alpha=alphas[0])
-        ax2.plot(x, values[1,:], color=colors[1], label=labels[1], alpha=alphas[1])
+        axis = [ax, ax2]
+        for value, color, label, alpha, axind in zip(values, colors, labels, alphas, axindex):
+            axis[axind].plot(x, value, color=color, label=label, alpha=alpha)
         ax.set_ylabel(y_labels[0])
         ax2.set_ylabel(y_labels[1])
-        ax2.legend()
+        
+        lines, labels = ax.get_legend_handles_labels()
+        lines2, labels2 = ax2.get_legend_handles_labels()
+        ax2.legend(lines + lines2, labels + labels2, loc=0)
     else:    
         for value, color, label, alpha in zip(values, colors, labels, alphas):
             ax.plot(x, value, color=color, label=label, alpha=alpha)
             ax.set_ylabel('Percolation outcome')
+            ax.legend()
     
-    ax.set_xlabel('Fraction of nodes removed')
-    ax.legend()
+    ax.set_xlabel('Fraction of edges/nodes removed')
+
+    if len(save_path) > 0:
+        plt.savefig(save_path, format='pdf',bbox_inches='tight')