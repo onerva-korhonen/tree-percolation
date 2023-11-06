added option for sustainability calculation + more visualization fixes

frontend.py
@@ -23,7 +23,7 @@ cfg['conduit_diameters'] = 'lognormal'#mrad_params.conduit_diameters
 conduit_elements, conns = mrad_model.create_mrad_network(cfg) # if no params are given, the function uses the default params of the Mrad model
 net = mrad_model.mrad_to_openpnm(conduit_elements, conns)
 #visualization.visualize_network_with_openpnm(net)
-net_cleaned = mrad_model.clean_network(net, conduit_elements, cfg['net_size'][0] - 1)
+net_cleaned, _ = mrad_model.clean_network(net, conduit_elements, cfg['net_size'][0] - 1)
 #visualization.visualize_network_with_openpnm(net_cleaned)
 
 #mrad_model.save_network(net_cleaned, params.network_save_file)
@@ -32,14 +32,17 @@ sim_net = mrad_model.prepare_simulation_network(net_cleaned, cfg)
 #visualization.visualize_pores(sim_net)
 #visualization.visualize_network_with_openpnm(sim_net, params.use_cylindrical_coordinates, mrad_params.Lce, 'pore.coords')
 effective_conductance = mrad_model.simulate_water_flow(sim_net, cfg, visualize=params.visualize_simulations)
-lcc_size = percolation.get_lcc_size(sim_net)
+lcc_size, susceptibility = percolation.get_lcc_size(sim_net)
 
 cfg['use_cylindrical_coords'] = False
 net_cleaned['pore.diameter'] = sim_net['pore.diameter']
-effective_conductances, lcc_sizes, functional_lcc_sizes = percolation.run_percolation(net_cleaned, cfg, percolation_type='bond', removal_order='random')
+effective_conductances, lcc_sizes, functional_lcc_sizes, nonfunctional_component_size, susceptibilities, functional_susceptibilities = percolation.run_percolation(net_cleaned, cfg, percolation_type='bond', removal_order='random')
 np.append(np.array([effective_conductance]), effective_conductances)
 np.append(np.array([lcc_size]), lcc_sizes)
 np.append(np.array([lcc_size]), functional_lcc_sizes)
+np.append(np.array([0]), nonfunctional_component_size)
+np.append(np.array([susceptibility]), susceptibilities)
+np.append(np.array([susceptibility]), functional_susceptibilities)
 percolation_outcome_values = np.concatenate((np.expand_dims(effective_conductances, axis=0), 
                                              np.expand_dims(lcc_sizes, axis=0), np.expand_dims(functional_lcc_sizes, axis=0)),
                                              axis=0)
@@ -47,4 +50,6 @@ visualization.plot_percolation_curve(net_cleaned['throat.conns'].shape[0], perco
                                      colors=params.percolation_outcome_colors, labels=params.percolation_outcome_labels, 
                                      alphas=params.percolation_outcome_alphas, y_labels=params.percolation_outcome_ylabels,
                                      axindex=params.percolation_outcome_axindex, save_path=params.percolation_plot_save_path)
+visualization.plot_percolation_curve(net_cleaned['throat.conns'].shape[0], np.expand_dims(nonfunctional_component_size, axis=0),
+                                     colors=['b'], labels=['total nonfunctional component size'], alphas=[1], save_path=params.nonfunctional_componen_size_save_path)
 

mrad_model.py
@@ -607,6 +607,9 @@ def clean_network(net, conduit_elements, outlet_row_index, remove_dead_ends=True
     -------
     net : openpnm.Network()
         the network object after cleaning
+    removed_components : list of lists
+        components removed because they are not connected to the inlet or to the outlet; each element corresponds to a 
+        removed component and contains the indices of the pores in the removed component
     """    
     A = net.create_adjacency_matrix(fmt='coo', triu=True) # the rows/columns of A correspond to conduit elements and values indicate the presence/absence of connections
     component_labels = csg.connected_components(A, directed=False)[1]
@@ -628,6 +631,7 @@ def clean_network(net, conduit_elements, outlet_row_index, remove_dead_ends=True
         if (in_btm == 0) or (in_top == 0):
             pores_to_remove.append(component)
     if len(pores_to_remove) > 0:
+        removed_components = pores_to_remove
         pores_to_remove = np.concatenate(pores_to_remove)
     
         conduit_elements = np.delete(conduit_elements, pores_to_remove, 0)
@@ -678,7 +682,7 @@ def clean_network(net, conduit_elements, outlet_row_index, remove_dead_ends=True
             conduit_degree_info = get_conduit_degree(conduit_elements, throat_conduits, outlet_row_index)
             conduits_to_remove = conduit_indices[np.where(conduit_degree_info[:, 3] == 1)]
         
-    return net
+    return net, removed_components
         
 def save_network(net, save_path):
     """

percolation.py
@@ -15,8 +15,6 @@ import scipy.sparse.csgraph as csg
 import mrad_model
 import mrad_params as params
 
-# TODO: write a function for calculating susceptibility
-
 def run_percolation(net, cfg, percolation_type='bond', removal_order='random'):
     """
     Removes throats (bond percoltion) or pores (site percolation) from an OpenPNM network object in a given (or random) order and calculates the effective conductance
@@ -62,8 +60,13 @@ def run_percolation(net, cfg, percolation_type='bond', removal_order='random'):
         size of the largest connected component after each removal
     functional_lcc_size : np.array
         the size of the largest functional component (i.e. component connected to both inlet and outlet) after each removal
+    nonfunctional_component_size : np.array
+        total size (i.e. sum of sizes) of non-functional components (i.e. components not connected to either inlet, outlet, or both) after each removal
+    susceptibility : np.array
+        susceptibility (i.e. the second-largest component size) after each removal
+    functional_susceptibility : np.array
+        functional susceptibility (i.e. the size of the second-largest component connected to inlet and outlet) after each removal
     """
-    #import pdb; pdb.set_trace()
     assert percolation_type in ['bond', 'site'], 'percolation type must be bond (removal of throats) or site (removal of pores)'
     if percolation_type == 'bond':
         n_removals = net['throat.conns'].shape[0]
@@ -74,6 +77,9 @@ def run_percolation(net, cfg, percolation_type='bond', removal_order='random'):
     effective_conductances = np.zeros(n_removals)
     lcc_size = np.zeros(n_removals)
     functional_lcc_size = np.zeros(n_removals)
+    nonfunctional_component_size = np.zeros(n_removals)
+    susceptibility = np.zeros(n_removals)
+    functional_susceptibility = np.zeros(n_removals)
     cfg['conduit_diameters'] = 'inherit_from_net'
     if removal_order == 'random':
         removal_order = np.arange(0, n_removals)
@@ -90,20 +96,27 @@ def run_percolation(net, cfg, percolation_type='bond', removal_order='random'):
                 op.topotools.trim(sim_net, pores=removal_order[:i + 1])
             except Exception as e:
                 if str(e) == 'Cannot delete ALL pores':
+                    nonfunctional_component_size[i] = len(net['pore.coords'])
                     continue # this would remove the last node from the network; at this point, value of all outputs should be 0
-        lcc_size[i] = get_lcc_size(sim_net)
+        lcc_size[i], susceptibility[i] = get_lcc_size(sim_net)
         try:
             conduit_elements = mrad_model.get_conduit_elements(sim_net, cec_indicator=cec_indicator)
-            sim_net = mrad_model.clean_network(sim_net, conduit_elements, cfg['net_size'][0] - 1, remove_dead_ends=False)
+            sim_net, removed_components = mrad_model.clean_network(sim_net, conduit_elements, cfg['net_size'][0] - 1, remove_dead_ends=False)
+            nonfunctional_component_size[i] = np.sum([len(removed_component) for removed_component in removed_components])
             sim_net = mrad_model.prepare_simulation_network(sim_net, cfg)
             effective_conductances[i] = mrad_model.simulate_water_flow(sim_net, cfg, visualize=False)
-            functional_lcc_size[i] = get_lcc_size(sim_net)
+            functional_lcc_size[i], functional_susceptibility[i] = get_lcc_size(sim_net)
         except Exception as e:
             if str(e) == 'Cannot delete ALL pores': # this is because all remaining nodes belong to non-functional components
                 effective_conductances[i] = 0  
                 functional_lcc_size[i] = 0
-        
-    return effective_conductances, lcc_size, functional_lcc_size
+                nonfunctional_component_size[i] = len(net['pore.coords'])
+            if (str(e) == "'throat.conns'") and (len(sim_net['throat.all']) == 0): # this is because all links have been removed from the network by op.topotools.trim
+                effective_conductances[i] = 0
+                functional_lcc_size[i] = 0
+                nonfunctional_component_size[i] = len(net['pore.coords'])
+    
+    return effective_conductances, lcc_size, functional_lcc_size, nonfunctional_component_size, susceptibility, functional_susceptibility
 
 def get_lcc_size(net):
     """
@@ -118,8 +131,9 @@ def get_lcc_size(net):
     -------
     lcc_size : int
         size of the largest connected component
+    susceptibility : int
+        size of the second-largest connected component
     """
-    # TODO: check what this returns if there are no nodes in the network; should return 0
     try:
         A = net.create_adjacency_matrix(fmt='coo', triu=True) # the rows/columns of A correspond to conduit elements and values indicate the presence/absence of connections
     except KeyError as e:
@@ -129,6 +143,10 @@ def get_lcc_size(net):
             else:
                 raise
     component_labels = csg.connected_components(A, directed=False)[1]
-    component_sizes = [len(np.where(component_labels == component_label)[0]) for component_label in np.unique(component_labels)]
-    lcc_size = np.amax(component_sizes)
-    return lcc_size
+    component_sizes = np.sort(np.unique([len(np.where(component_labels == component_label)[0]) for component_label in np.unique(component_labels)]))
+    lcc_size = component_sizes[-1]
+    if len(component_sizes) > 1:
+        susceptibility = component_sizes[-2]
+    else:
+        susceptibility = 0
+    return lcc_size, susceptibility