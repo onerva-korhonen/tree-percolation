b6e7e039a2989eb72ff129b027b84caba2e8dbe7
Korhonen Onerva <aokorhon@login3.triton.aalto.fi>
2024-01-15 17:39:35 +0200

    changed params + removed visualization from frontend

--- frontend.py
+++ frontend.py
@@ -10,21 +10,22 @@ A frontend script for investigating percolation using the Mrad et al. xylem netw
 import mrad_model
 import mrad_params
 import params
-import visualization
+#import visualization
 import percolation
 
 import openpnm as op
 import numpy as np
+import pickle
 
 cfg = {}
-cfg['net_size'] = mrad_params.net_size
+cfg['net_size'] = params.net_size
 cfg['conduit_diameters'] = 'lognormal'#mrad_params.conduit_diameters
 cfg['Dc'] = params.Dc
 cfg['Dc_cv'] = params.Dc_cv
-cfg['seeds_NPc'] = mrad_params.seeds_NPc
-cfg['seeds_Pc'] = mrad_params.seeds_Pc
-cfg['seed_ICC_rad'] = mrad_params.seed_ICC_rad
-cfg['seed_ICC_tan'] = mrad_params.seed_ICC_tan
+cfg['seeds_NPc'] = params.seeds_NPc
+cfg['seeds_Pc'] = params.seeds_Pc
+cfg['seed_ICC_rad'] = params.seed_ICC_rad
+cfg['seed_ICC_tan'] = params.seed_ICC_tan
 cfg['si_type'] = params.si_type
 cfg['si_length'] = params.si_length
 cfg['start_conduit'] = params.start_conduit
@@ -33,17 +34,19 @@ cfg['spreading_threshold'] = params.spreading_threshold
 
 cfg['fixed_random'] = True
 
+print(cfg['net_size'])
+
 conduit_elements, conns = mrad_model.create_mrad_network(cfg) # if no params are given, the function uses the default params of the Mrad model
 net = mrad_model.mrad_to_openpnm(conduit_elements, conns)
-visualization.visualize_network_with_openpnm(net)
+#visualization.visualize_network_with_openpnm(net)
 net_cleaned, _ = mrad_model.clean_network(net, conduit_elements, cfg['net_size'][0] - 1)
-visualization.visualize_network_with_openpnm(net_cleaned)
+#visualization.visualize_network_with_openpnm(net_cleaned)
 
 #mrad_model.save_network(net_cleaned, params.network_save_file)
 
 sim_net = mrad_model.prepare_simulation_network(net_cleaned, cfg)
-visualization.visualize_pores(sim_net)
-visualization.visualize_network_with_openpnm(sim_net, params.use_cylindrical_coordinates, mrad_params.Lce, 'pore.coords')
+#visualization.visualize_pores(sim_net)
+#visualization.visualize_network_with_openpnm(sim_net, params.use_cylindrical_coordinates, mrad_params.Lce, 'pore.coords')
 effective_conductance = mrad_model.simulate_water_flow(sim_net, cfg, visualize=params.visualize_simulations)
 if params.percolation_type in ['conduit', 'si']:
     lcc_size, susceptibility, _ = percolation.get_conduit_lcc_size(sim_net)
@@ -78,16 +81,22 @@ if params.percolation_type == 'si':
     x = np.append(np.array([0]), prevalence)
 else:
     x = []
-visualization.plot_percolation_curve(total_n_nodes, percolation_outcome_values,
-                                     colors=params.percolation_outcome_colors, labels=params.percolation_outcome_labels, 
-                                     alphas=params.percolation_outcome_alphas, y_labels=params.percolation_outcome_ylabels,
-                                     axindex=params.percolation_outcome_axindex, save_path=params.percolation_plot_save_path, x=x)
-visualization.plot_percolation_curve(total_n_nodes, np.expand_dims(nonfunctional_component_volume, axis=0),
-                                     colors=[params.percolation_nonfunctional_component_size_color], labels=[params.percolation_nonfunctional_component_size_label], 
-                                     alphas=[params.percolation_nonfunctional_component_size_alpha], save_path=params.nonfunctional_componen_size_save_path, x=x)
-visualization.plot_percolation_curve(total_n_nodes, 
-                                     np.concatenate((np.expand_dims(n_inlets, axis=0), np.expand_dims(n_outlets, axis=0)), axis=0),
-                                     colors=[params.percolation_ninlet_color, params.percolation_noutlet_color],
-                                     labels=[params.percolation_ninlet_label, params.percolation_noutlet_label],
-                                     alphas=[params.percolation_ninlet_alpha, params.percolation_noutlet_alpha],
-                                     save_path=params.ninlet_save_path, x=x)
+
+data = {'effective_conductances':effective_conductances, 'lcc_sizes':lcc_sizes, 'functional_lcc_sizes':functional_lcc_sizes, 'nonfunctional_component_size':nonfunctional_component_size, 'susceptibilities':susceptibilities, 'functional_susceptibilities':functional_susceptibilities, 'n_inlets':n_inlets, 'n_outlets': n_outlets, 'nonfunctional_component_volume':nonfunctional_component_volume, 'total_n_nodes':total_n_nodes, 'x':x}
+f = open(params.percolation_data_save_path, 'wb')
+pickle.dump(data, f)
+f.close()
+
+#visualization.plot_percolation_curve(total_n_nodes, percolation_outcome_values,
+#                                     colors=params.percolation_outcome_colors, labels=params.percolation_outcome_labels, 
+#                                     alphas=params.percolation_outcome_alphas, y_labels=params.percolation_outcome_ylabels,
+#                                     axindex=params.percolation_outcome_axindex, save_path=params.percolation_plot_save_path, x=x)
+#visualization.plot_percolation_curve(total_n_nodes, np.expand_dims(nonfunctional_component_volume, axis=0),
+#                                     colors=[params.percolation_nonfunctional_component_size_color], labels=[params.percolation_nonfunctional_component_size_label], 
+#                                     alphas=[params.percolation_nonfunctional_component_size_alpha], save_path=params.nonfunctional_componen_size_save_path, x=x)
+#visualization.plot_percolation_curve(total_n_nodes, 
+#                                     np.concatenate((np.expand_dims(n_inlets, axis=0), np.expand_dims(n_outlets, axis=0)), axis=0),
+#                                     colors=[params.percolation_ninlet_color, params.percolation_noutlet_color],
+#                                     labels=[params.percolation_ninlet_label, params.percolation_noutlet_label],
+#                                     alphas=[params.percolation_ninlet_alpha, params.percolation_noutlet_alpha],
+#                                     save_path=params.ninlet_save_path, x=x)
---
+++ visualization_frontend.py
@@ -0,0 +1,42 @@
+# a script for visualizing percolation outcomes based on the percolation data calculated with slurm
+
+import params
+import visualization
+
+import pickle
+import numpy as np
+
+f = open(params.percolation_data_save_path, 'rb')
+data = pickle.load(f)
+f.close()
+
+effective_conductances = data['effective_conductances']
+lcc_sizes = data['lcc_sizes']
+functional_lcc_sizes = data['functional_lcc_sizes']
+nonfunctional_component_size = data['nonfunctional_component_size']
+susceptibilities = data['susceptibilities']
+functional_susceptibilities = data['functional_susceptibilities']
+n_inlets = data['n_inlets']
+n_outlets = data['n_outlets']
+nonfunctional_component_volume = data['nonfunctional_componennt_volume'] # TODO: fix the typo
+total_n_nodes = data['total_n_nodes']
+x = data['x']
+
+percolation_outcome_values = np.concatenate((np.expand_dims(effective_conductances, axis=0), 
+                                             np.expand_dims(lcc_sizes, axis=0), np.expand_dims(functional_lcc_sizes, axis=0)),
+                                             axis=0)
+
+visualization.plot_percolation_curve(total_n_nodes, percolation_outcome_values,
+                                     colors=params.percolation_outcome_colors, labels=params.percolation_outcome_labels, 
+                                     alphas=params.percolation_outcome_alphas, y_labels=params.percolation_outcome_ylabels,
+                                     axindex=params.percolation_outcome_axindex, save_path=params.percolation_plot_save_path, x=x)
+visualization.plot_percolation_curve(total_n_nodes, np.expand_dims(nonfunctional_component_volume, axis=0),
+                                     colors=[params.percolation_nonfunctional_component_size_color], labels=[params.percolation_nonfunctional_component_size_label], 
+                                     alphas=[params.percolation_nonfunctional_component_size_alpha], save_path=params.nonfunctional_componen_size_save_path, x=x)
+visualization.plot_percolation_curve(total_n_nodes, 
+                                     np.concatenate((np.expand_dims(n_inlets, axis=0), np.expand_dims(n_outlets, axis=0)), axis=0),
+                                     colors=[params.percolation_ninlet_color, params.percolation_noutlet_color],
+                                     labels=[params.percolation_ninlet_label, params.percolation_noutlet_label],
+                                     alphas=[params.percolation_ninlet_alpha, params.percolation_noutlet_alpha],
+                                     save_path=params.ninlet_save_path, x=x)
+
