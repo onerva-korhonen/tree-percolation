fixed some bugs in the calculation of resampled RSME

scripts/visualize_single_param_spreading.py
@@ -44,19 +44,19 @@ def rmse(x1, x2): # helper function for calculating RMSE
         x2_padded = x2
         x1_cut = x1
         x2_cut = x2[:len(x1)]
-        x1_interp = x1
-        x2_interp = np.interp(np.arange(len(x1)), np.arange(max_dim), x2)
+        x1_resampled = x1
+        x2_resampled = np.interp(np.arange(len(x1)), np.arange(max_dim), x2)
     elif len(x2) < max_dim:
         x2_padded = np.concatenate((x2, np.ones(max_dim - len(x2)) * x2[-1]), axis=0)
         x1_padded = x1
         x1_cut = x1[:len(x2)]
         x2_cut = x2
-        x1_interp = np.interp(np.arange(len(x2)), np.arange(max_dim), x1)
-        x2_interp = x2
+        x1_resampled = np.interp(np.arange(len(x2)), np.arange(max_dim), x1)
+        x2_resampled = x2
     rmse_cut = np.sqrt(np.sum((x1_cut - x2_cut)**2) / len(x1_cut))
     rmse_padded = np.sqrt(np.sum((x1_padded - x2_padded)**2) / len(x2_padded))
-    rmse_interp = np.sqrt(np.sum((x1_interp - x2_interp)**2) / len(x2_interp))
-    return rmse_cut, rmse_padded
+    rmse_resampled = np.sqrt(np.sum((x1_resampled - x2_resampled)**2) / len(x2_resampled))
+    return rmse_cut, rmse_padded, rmse_resampled
 
 if __name__=='__main__':
 
@@ -287,22 +287,22 @@ if __name__=='__main__':
  
             # 3) Calculating RMSE
 
-            rmse_cut_prevalence, rmse_padded_prevalence, rmse_interp_prevalence = rmse(av_phys_prevalence, av_stoch_prevalence)
-            rmse_cut_eff_conductance, rmse_padded_eff_conductance, rmse_interp_eff_conductance = rmse(av_phys_effective_conductances, av_stoch_effective_conductances)
-            rmse_cut_lcc_size, rmse_padded_lcc_size, rmse_interp_lcc_size = rmse(av_phys_lcc_size, av_stoch_lcc_size)
-            rmse_cut_func_lcc_size, rmse_padded_func_lcc_size, rmse_interp_func_lcc_size = rmse(av_phys_func_lcc_size, av_stoch_func_lcc_size)
-            rmse_cut_nonfunc_volume, rmse_padded_nonfunc_volume, rmse_interp_nonfunc_volume = rmse(av_phys_nonfunc_volume, av_stoch_nonfunc_volume)
-            rmse_cut_ninlets, rmse_padded_ninlets, rmse_interp_ninlets = rmse(av_phys_ninlets, av_stoch_ninlets)
-            rmse_cut_noutlets, rmse_padded_noutlets, rmse_interp_noutlets = rmse(av_phys_noutlets, av_stoch_noutlets)
+            rmse_cut_prevalence, rmse_padded_prevalence, rmse_resampled_prevalence = rmse(av_phys_prevalence, av_stoch_prevalence)
+            rmse_cut_eff_conductance, rmse_padded_eff_conductance, rmse_resampled_eff_conductance = rmse(av_phys_effective_conductances, av_stoch_effective_conductances)
+            rmse_cut_lcc_size, rmse_padded_lcc_size, rmse_resampled_lcc_size = rmse(av_phys_lcc_size, av_stoch_lcc_size)
+            rmse_cut_func_lcc_size, rmse_padded_func_lcc_size, rmse_resampled_func_lcc_size = rmse(av_phys_func_lcc_size, av_stoch_func_lcc_size)
+            rmse_cut_nonfunc_volume, rmse_padded_nonfunc_volume, rmse_resampled_nonfunc_volume = rmse(av_phys_nonfunc_volume, av_stoch_nonfunc_volume)
+            rmse_cut_ninlets, rmse_padded_ninlets, rmse_resampled_ninlets = rmse(av_phys_ninlets, av_stoch_ninlets)
+            rmse_cut_noutlets, rmse_padded_noutlets, rmse_resampled_noutlets = rmse(av_phys_noutlets, av_stoch_noutlets)
  
             print(f'At pressure difference {pressure_difference}, optimal SI spreading probability is {spreading_probability}')
-            print(f'Prevalence: cut RMSE: {rmse_cut_prevalence}, padded RMSE: {rmse_padded_prevalence}, interpolated RMSE: {rmse_interp_prevalence}')
-            print(f'Effective conductance: cut RMSE: {rmse_cut_eff_condudctance}, padded RMSE: {rmse_padded_eff_conductance}, interpolated RMSE: {rmse_interp_eff_conductance}')
-            print(f'LCC size: cut RMSE: {rmse_cut_lcc_size}, padded RMSE: {rmse_padded_lcc_size}, interpolated RMSE: {rmse_interp_lcc_size')
-            print(f'Functional LCC size: cut RMSE: {rmse_cut_func_lcc_size}, padded RMSE: {rmse_padded_func_lcc_size}, interpolated RMSE: {rmse_interp_func_lcc_size')
-            print(f'Nonfunctional component volume: cut RMSE: {rmse_cut_nonfunc_volume}, padded RMSE: {rmse_padded_nonfunc_volume}, interpolated_RMSE: {rmse_interp_nonfunc_volume}')
-            print(f'N inlets: cut RMSE: {rmse_cut_ninlets}, padded RMSE: {rmse_padded_ninlets}, interpolated RMSE: {rmse_interp_ninlets}')
-            print(f'N outlets: cut RMSE: {rmse_cut_noutlets}, padded RMSE: {rmse_padded_noutlets}, interpolated RMSE: {rmse_interp_noutlets}')
+            print(f'Prevalence: cut RMSE: {rmse_cut_prevalence}, padded RMSE: {rmse_padded_prevalence}, resampled RMSE: {rmse_resampled_prevalence}')
+            print(f'Effective conductance: cut RMSE: {rmse_cut_eff_conductance}, padded RMSE: {rmse_padded_eff_conductance}, resampled RMSE: {rmse_resampled_eff_conductance}')
+            print(f'LCC size: cut RMSE: {rmse_cut_lcc_size}, padded RMSE: {rmse_padded_lcc_size}, resampled RMSE: {rmse_resampled_lcc_size}')
+            print(f'Functional LCC size: cut RMSE: {rmse_cut_func_lcc_size}, padded RMSE: {rmse_padded_func_lcc_size}, resampled RMSE: {rmse_resampled_func_lcc_size}')
+            print(f'Nonfunctional component volume: cut RMSE: {rmse_cut_nonfunc_volume}, padded RMSE: {rmse_padded_nonfunc_volume}, resampled RMSE: {rmse_resampled_nonfunc_volume}')
+            print(f'N inlets: cut RMSE: {rmse_cut_ninlets}, padded RMSE: {rmse_padded_ninlets}, resampled RMSE: {rmse_resampled_ninlets}')
+            print(f'N outlets: cut RMSE: {rmse_cut_noutlets}, padded RMSE: {rmse_padded_noutlets}, resampled RMSE: {rmse_resampled_noutlets}')
 
 
 